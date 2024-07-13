90c4ea35589641e153ec3ba3180305732a7d2f9e
Mac <mac@Macs-MacBook-Pro.local>
2024-07-13 21:45:17 +0100

    first commit

---
+++ style.js
@@ -0,0 +1,29 @@
+/** @format */
+
+const iconOpen = document.querySelector('.icon-open')
+const hamburger = document.querySelector('.hamburger-container')
+const iconClose = document.querySelector('.icon-close')
+const html = document.documentElement
+
+function OpenHamburger() {
+	hamburger.style.transform = 'scaleY(1)'
+	iconClose.style.display = 'block'
+	iconOpen.style.display = 'none'
+}
+function closeHamburger() {
+	hamburger.style.transform = 'scaleY(0)'
+	iconClose.style.display = 'none'
+	iconOpen.style.display = 'block'
+	html.removeEventListener(CloseHamburgerOnBodyClick)
+}
+// document.addEventListener('click', (e) => {
+// 	const target = e.target
+// 	if (!hamburger.contains(target) && !iconOpen.contains(target)) {
+// 		closeHamburger()
+// 	} 
+// })
+
+iconOpen.addEventListener('click', OpenHamburger)
+
+iconClose.addEventListener('click', closeHamburger)
+
